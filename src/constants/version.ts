1fc46fb69c5568e47201b54b3d181ffc26a73c74
零泠丶 <632177447@qq.com>
2026-09-09 17:16:14 +0800

    chore: 升级项目各模块版本号至1.0.0并同步配置

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -3,4 +3,4 @@
  * 在 Vite 构建时由 package.json 的 version 自动注入
  */
 export const APP_VERSION: string =
-  typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : 'v0.2.0';
+  typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : 'v1.0.0';
